4c129e64dbb219245f4402ec4c377b4beb6f2ea4
rbardini <raphael.bardini@etudiant.univ-rennes1.fr>
2024-06-10 15:30:18 +0200

    création et modification de catégorie

--- lot10/SAE/src/controleur/Categorie.java
+++
@@ -1,9 +0,0 @@
-package controleur;
-
-import modele.CategorieSpectateur;
-
-public class Categorie {
-	public void afficher(CategorieSpectateur categorie) {
-		
-	}
-}
--- lot10/SAE/src/vue/FenCreationCategorie.java
+++ lot10/SAE/src/vue/FenCreationCategorie.java
@@ -8,10 +8,10 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import modele.CategorieSpectateur;
 
-public class CreationCategorie extends Stage {
-	controleur.Categorie ctrl;
+public class FenCreationCategorie extends Stage {
+	private controleur.CreationCategorie ctrl;
 
-	public CreationCategorie() throws IOException {
+	public FenCreationCategorie() throws IOException {
 		this.setTitle("Créer une catégorie");
 		this.setResizable(false);
 		Scene scene = new Scene(creerSceneGraph());
@@ -21,10 +21,14 @@ public class CreationCategorie extends Stage {
 	public void afficher(CategorieSpectateur categorie) {
 		ctrl.afficher(categorie);
 	}
+	
+	public controleur.CreationCategorie ctrl() {
+		return ctrl;
+	}
 
 	private Pane creerSceneGraph() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("imprimer.fxml"));
+		loader.setLocation(getClass().getResource("creationCategorie.fxml"));
 		Pane root = loader.load();
 		ctrl = loader.getController();
 		return root;
