7ef8d1bc95b90f31a2c1b7a07a8e59d7b8e1dc5d
rbardini <raphael.bardini@etudiant.univ-rennes1.fr>
2024-06-10 14:23:31 +0200

    Categories de spectateurs

--- lot10/SAE/src/modele/CategorieSpectateur.java
+++ lot10/SAE/src/modele/CategorieSpectateur.java
@@ -1,10 +1,22 @@
 package modele;
 
+import java.util.Collections;
+import java.util.List;
 
 public final record CategorieSpectateur( 
     String nom,
     String description,
-    double reduction // todo: il faudrait utiliser un type rationnel pour éviter les erreurs d'arrondi
+    double reduction
 ) {
- 
+	// Modification par raport à la proposition de correction du diagramme de classe
+	// Au lieu d'une hiérarchie de Tarification, on utilise des variables de classes pour représenter les catégories de base
+	/**
+	 * Liste non modifiable des catégories de base.
+	 */
+	public static final List<CategorieSpectateur> CATEGORIES_DE_BASE = Collections.unmodifiableList(List.of(
+		new CategorieSpectateur("Jeune", "Personne de moins de 25 ans", .3),
+		new CategorieSpectateur("Sénior", "Perosnne de plus de 65 ans", .25),
+		new CategorieSpectateur("Abonné", "Personne qui assiste à au moins 3 spectacles par an", .3),
+		new CategorieSpectateur("Groupe", "Personne membre d'un groupe d'au moins 20 adultes", .15),
+		new CategorieSpectateur("Adulte", "Personne n'appartenant à aucune autre catégorie", 0)));
 }
