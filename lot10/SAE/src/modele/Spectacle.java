0f6eca3afe9d521d29f63c57ebab704f0b080eb1
Paolo TOE <paolo.toe@etudiant.univ-rennes1.fr>
2024-06-10 15:20:35 +0200

    Epic work done

--- lot10/SAE/src/modele/Spectacle.java
+++ lot10/SAE/src/modele/Spectacle.java
@@ -9,20 +9,136 @@ public final class Spectacle {
 	private int nbreMaxSpect;
 	private String genre;
 	private ArrayList<Artiste> listeArtiste;
-	private ArrayList<HashMap<Zone, Float>>  tarifs;
+	private HashMap<Zone, Float>  tarifs; // Hashmap qui lie pour une ou plusieures Zones un tarif pour cette instance
 	private CategorieSpectacle categorie;
-	
-	public Spectacle(String n, int d, int nb, String g, Artiste art, CategorieSpectacle categorie) {
+
+	public Spectacle(String n, int d, int nb, String g, Artiste art, CategorieSpectacle cat) {
 		nom = n;
 		duree =d;
 		nbreMaxSpect = nb;
 		genre = g;
-		this.categorie = categorie;
-		
+		categorie = cat;
+
 		listeArtiste = new ArrayList<Artiste>();
 		listeArtiste.add(art);
 		
-		tarifs = new ArrayList<HashMap<Zone, Float>>();
-		
+		tarifs = new HashMap<Zone, Float>();
+	}
+
+	public void afficher(){
+		System.out.println("Nom spectacle : " + nom);
+		System.out.println("Durée : " + duree);
+		System.out.println("Nombre maximum de spectateurs : " + nbreMaxSpect);
+		System.out.println("Genre du spectacle : " + genre);
+		System.out.println("Catégorie de spectacle : " + categorie);
+	}
+
+	public String toString() {
+		return nom;
+	}
+
+	public void listerArtistes(){
+		for (Artiste art : listeArtiste) {
+			art.afficher();
+		}
+	}
+
+	public void listerTarifs() throws IllegalStateException{
+		if (!tarifs.isEmpty()) {
+			for (Zone z : tarifs.keySet()) {
+				System.out.println(z);
+				System.out.println(tarifs.get(z) + "\n");
+			}
+		}else{
+			throw new IllegalStateException();
+		}
+	}
+
+	public void ajouterArtiste(Artiste a) throws IllegalArgumentException{
+		if (a!=null) {
+			listeArtiste.add(a);
+		}else{
+			throw new IllegalArgumentException();
+		}
+	}
+	public void retirerArtiste(Artiste a) throws IllegalArgumentException{
+		if (a!=null && listeArtiste.contains(a)) {
+			listeArtiste.remove(a);
+		}else{
+			throw new IllegalArgumentException();
+		}
+	}
+
+	// Tarifs
+	public void ajouterTarif(Zone z, Float f)throws IllegalArgumentException{
+		if ((!tarifs.containsKey(z))&&(z != null)) {
+			putTarif(z, f);
+			z.putTarif(this, f);
+		}else{
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public void retirerTarif(Zone z) throws IllegalArgumentException{
+		if ((tarifs.containsKey(z))&&(z != null)) {
+			removeTarif(z);
+			z.removeTarif(this);
+		}else{
+			throw new IllegalArgumentException();
+		}
+	}
+
+	// pour utilisation externe (liaison bi-directionelle)
+	public void putTarif(Zone z, Float f){
+		tarifs.put(z, f);
+	}
+	
+	public void removeTarif(Zone z){
+		tarifs.remove(z);
+	}
+
+	/////Getters and setters
+
+	public CategorieSpectacle getCategorie() {
+		return categorie;
+	}
+	public int getDuree() {
+		return duree;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public ArrayList<Artiste> getListeArtiste() {
+		return listeArtiste;
+	}
+	public int getNbreMaxSpect() {
+		return nbreMaxSpect;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public HashMap<Zone, Float> getTarifs() {
+		return tarifs;
+	}
+	public void setTarifs(HashMap<Zone, Float> tarifs) {
+		this.tarifs = tarifs;
+	}
+	public void setCategorie(CategorieSpectacle categorie) {
+		this.categorie = categorie;
+	}
+	public void setDuree(int duree) {
+		this.duree = duree;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public void setListeArtiste(ArrayList<Artiste> listeArtiste) {
+		this.listeArtiste = listeArtiste;
+	}
+	public void setNbreMaxSpect(int nbreMaxSpect) {
+		this.nbreMaxSpect = nbreMaxSpect;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
 	}
 }
--- lot10/SAE/src/modele/Zone.java
+++ lot10/SAE/src/modele/Zone.java
@@ -1,18 +1,64 @@
 package modele;
 
 import java.util.HashMap;
-import java.util.ArrayList;
 
 public final class Zone {
 	private String nom;
 	
-	private ArrayList<HashMap<Spectacle, Float>>  tarifs;
+	private HashMap<Spectacle, Float>  tarifs;
 	
 	public Zone(String n) {
 		nom = n;
 		
-		tarifs = new ArrayList<HashMap<Spectacle, Float>>();
+		tarifs = new HashMap<Spectacle, Float>();
+	}
+
+	public void ajouterTarif(Spectacle s, Float f)throws IllegalArgumentException{
+		if (!tarifs.containsKey(s)&&(s != null)) {
+			putTarif(s, f);
+			s.putTarif(this, f);
+		}else{
+			throw new IllegalArgumentException();
+		}
+	}
+
+
+	// Tarifs
+	public void retirerTarif(Spectacle s) throws IllegalArgumentException{
+		if (tarifs.containsKey(s)&&(s != null)) {
+			removeTarif(s);
+			s.removeTarif(this);
+		}else{
+			throw new IllegalArgumentException();
+		}
 	}
 	
+	// pour utilisation externe (liaison bi-directionelle)
+	public void putTarif(Spectacle s, Float f){
+		tarifs.put(s, f);
+	}
 	
+	public void removeTarif(Spectacle s){
+		tarifs.remove(s);
+	}
+
+
+
+	public String toString() {
+		return nom;
+	}
+
+	// Getterss et setters
+	public String getNom() {
+		return nom;
+	}
+	public HashMap<Spectacle, Float> getTarifs() {
+		return tarifs;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public void setTarifs(HashMap<Spectacle, Float> tarifs) {
+		this.tarifs = tarifs;
+	}
 }
