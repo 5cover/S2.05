6942a6201be27bafdccae98d986c1e1bfe469237
marius <icelos@yahoo.com>
2024-06-14 18:13:54 +0200

    modif donne et liste spectacle

--- lot10/SAE/src/controleur/CtrlListeSpectacles.java
+++ lot10/SAE/src/controleur/CtrlListeSpectacles.java
@@ -18,84 +18,117 @@ import modele.Spectacle;
 public class CtrlListeSpectacles {
 
     @FXML
-    private static  TextField txt_numSpectateur = new TextField() ;
+    private   TextField txt_numSpectateur = new TextField() ;
     @FXML
-    private static TextField num_prixOrchestre = new TextField();
+    private  TextField num_prixOrchestre = new TextField();
     @FXML
-    private static TextField num_prixLogesDroites = new TextField();
+    private  TextField num_prixLogesDroites = new TextField();
     @FXML
-    private static TextField txt_duree = new TextField();
+    private  TextField txt_duree = new TextField();
     @FXML
-    private static TextField num_prixLogesGauches = new TextField();
+    private  TextField num_prixLogesGauches = new TextField();
     @FXML
-    private static TextField num_prixBalcon = new TextField();
+    private  TextField num_prixBalcon = new TextField();
     @FXML
-    private static TextField txt_nomSpectacle = new TextField();
+    private  TextField txt_nomSpectacle = new TextField();
     @FXML
-    private static TextField txt_genreSpectacle = new TextField();
+    private  TextField txt_genreSpectacle = new TextField();
     @FXML
-    private static TextField txt_categorieSpectacle = new TextField();
+    private  TextField txt_categorieSpectacle = new TextField();
     @FXML
-    private static TableView<Spectacle> tvListeSpectacle = new TableView<Spectacle>();
+    private  TableView<Spectacle> tvListeSpectacle = new TableView<Spectacle>();
 
     @FXML
-    private static TableColumn<Spectacle, Number> txt_colonneDuree/* = new TableColumn<Spectacle, Integer>()*/;
+    private  TableColumn<Spectacle, Number> txt_colonneDuree;
     @FXML
-    private static TableColumn<Spectacle, Number> txt_colonneCapacite/* = new TableColumn<Spectacle, Integer>()*/;
+    private  TableColumn<Spectacle, Number> txt_colonneCapacite;
     @FXML
-    private static TableColumn<Spectacle, String> txt_colonneGenre/* = new TableColumn<Spectacle, String>()*/;
+    private  TableColumn<Spectacle, String> txt_colonneGenre;
     @FXML
-    private static TableColumn<Spectacle, String> txt_colonneCategorie/* = new TableColumn<Spectacle, String>()*/;
+    private  TableColumn<Spectacle, String> txt_colonneCategorie;
     @FXML
-    private static TableColumn<Spectacle, String> txt_colonneNom/* = new TableColumn<Spectacle, String>()*/;
+    private  TableColumn<Spectacle, String> txt_colonneNom;
 
-    public static Spectacle spectSelected;
-
-//    Person person = taview.getSelectionModel().getSelectedItem();
-    public static void actualiser() {
-        spectSelected = tvListeSpectacle.getSelectionModel().getSelectedItem();
-        if (spectSelected == null) {
-            return;
-        }
-        txt_numSpectateur.setText(Integer.toString(spectSelected.getCapacite()));
-        num_prixOrchestre.setText(Float.toString(spectSelected.tarifZone(Donnees.getTarifsZoneDroite())));
-        num_prixLogesDroites.setText(Float.toString(spectSelected.tarifZone(Donnees.getTarifsZoneOcherstre())));
-        txt_duree.setText(Integer.toString(spectSelected.getDuree()));
-        num_prixLogesGauches.setText(Float.toString(spectSelected.tarifZone(Donnees.getTarifsZoneGauche())));
-        num_prixBalcon.setText(Float.toString(spectSelected.tarifZone(Donnees.getTarifsZoneBalcon())));
-        txt_nomSpectacle.setText(spectSelected.getNom());
-        txt_genreSpectacle.setText(spectSelected.getGenre());
-        txt_categorieSpectacle.setText(spectSelected.getCategorie().toString());
-
-    }
-
+    @FXML
     void initialize() {
-//        TableColumn<Spectacle, String> colonne1 = new TableColumn<>("Nom");
-//        colonne1.setCellValueFactory(new PropertyValueFactory<>("nom"));
-//        tvListeSpectacle.getColumns().set(0, colonne1);
-        
+       
         txt_colonneNom.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getNom()));
         txt_colonneCategorie.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getCategorie().toString()));
         txt_colonneGenre.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getGenre()));
         txt_colonneCapacite.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().getCapacite()));
         txt_colonneDuree.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().getDuree()));
         
-//        TableColumn<Spectacle, String> colonne2 = new TableColumn<>("Categorie");
-//        colonne2.setCellValueFactory(new PropertyValueFactory<>("categorie"));
-//        tvListeSpectacle.getColumns().set(1, colonne2);
-//        TableColumn<Spectacle, Integer> colonne3 = new TableColumn<>("Capacite");
-//        colonne3.setCellValueFactory(new PropertyValueFactory<>("capacite"));
-//        tvListeSpectacle.getColumns().set(2, colonne3);
-//        TableColumn<Spectacle, Integer> colonne4 = new TableColumn<>("Duree");
-//        colonne4.setCellValueFactory(new PropertyValueFactory<>("duree"));
-//        tvListeSpectacle.getColumns().set(3, colonne4);
-//        TableColumn<Spectacle, String> colonne5 = new TableColumn<>("Genre");
-//        colonne5.setCellValueFactory(new PropertyValueFactory<>("genre"));
-//        tvListeSpectacle.getColumns().set(4, colonne5);
 
+        
+
+        txt_nomSpectacle.textProperty().bind(
+                Bindings.createStringBinding(() -> {
+                	Spectacle spectSelected = tvListeSpectacle.getSelectionModel().getSelectedItem();
+                    return spectSelected != null ? spectSelected.getNom() : "";
+                }, tvListeSpectacle.getSelectionModel().selectedItemProperty())
+        );
+        
+        txt_genreSpectacle.textProperty().bind(
+                Bindings.createStringBinding(() -> {
+                	Spectacle spectSelected = tvListeSpectacle.getSelectionModel().getSelectedItem();
+                    return spectSelected != null ? spectSelected.getGenre() : "";
+                }, tvListeSpectacle.getSelectionModel().selectedItemProperty())
+        );
+        
+        txt_categorieSpectacle.textProperty().bind(
+                Bindings.createStringBinding(() -> {
+                	Spectacle spectSelected = tvListeSpectacle.getSelectionModel().getSelectedItem();
+                    return spectSelected != null ? spectSelected.getCategorie().toString() : "";
+                }, tvListeSpectacle.getSelectionModel().selectedItemProperty())
+        );
+        
+        num_prixOrchestre.textProperty().bind(
+                Bindings.createStringBinding(() -> {
+                    Spectacle spectSelected = tvListeSpectacle.getSelectionModel().getSelectedItem();
+                    return spectSelected != null ? Float.toString(spectSelected.tarifZone(Donnees.getTarifsZoneOcherstre())) : "";
+                }, tvListeSpectacle.getSelectionModel().selectedItemProperty())
+        );
+        
+        num_prixLogesDroites.textProperty().bind(
+                Bindings.createStringBinding(() -> {
+                    Spectacle spectSelected = tvListeSpectacle.getSelectionModel().getSelectedItem();
+                    return spectSelected != null ? Float.toString(spectSelected.tarifZone(Donnees.getTarifsZoneDroite())) : "";
+                }, tvListeSpectacle.getSelectionModel().selectedItemProperty())
+        );
+        
+        
+        num_prixLogesGauches.textProperty().bind(
+                Bindings.createStringBinding(() -> {
+                    Spectacle spectSelected = tvListeSpectacle.getSelectionModel().getSelectedItem();
+                    return spectSelected != null ? Float.toString(spectSelected.tarifZone(Donnees.getTarifsZoneGauche())) : "";
+                }, tvListeSpectacle.getSelectionModel().selectedItemProperty())
+        );
+        
+        
+        
+        num_prixBalcon.textProperty().bind(
+                Bindings.createStringBinding(() -> {
+                    Spectacle spectSelected = tvListeSpectacle.getSelectionModel().getSelectedItem();
+                    return spectSelected != null ? Float.toString(spectSelected.tarifZone(Donnees.getTarifsZoneBalcon())) : "";
+                }, tvListeSpectacle.getSelectionModel().selectedItemProperty())
+        );
+        txt_duree.textProperty().bind(
+                Bindings.createStringBinding(() -> {
+                    Spectacle spectSelected = tvListeSpectacle.getSelectionModel().getSelectedItem();
+                    return spectSelected != null ? Integer.toString(spectSelected.getDuree()) : "";
+                }, tvListeSpectacle.getSelectionModel().selectedItemProperty())
+        );
+        txt_numSpectateur.textProperty().bind(
+                Bindings.createStringBinding(() -> {
+                    Spectacle spectSelected = tvListeSpectacle.getSelectionModel().getSelectedItem();
+                    return spectSelected != null ?Integer.toString( (spectSelected.getCapacite())) : "";
+                }, tvListeSpectacle.getSelectionModel().selectedItemProperty())
+        );
         tvListeSpectacle.setItems(Donnees.getLesSpectacle());
         tvListeSpectacle.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-
+        
+        
+        
     }
 
 }
